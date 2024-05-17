cc78227f104ff0f4c99b63dabd69c35e7dee952e
bishnu06mama <bishnu06mama@gmail.com>
2024-05-16 18:32:27 -0800

    new file added

---
+++ javaScript/class-1/01_variable.js
@@ -0,0 +1,37 @@
+// Declare a variable
+
+let num= 10;
+console.log("The output is:", num); // to print the output
+
+let Num = 1000;
+console.log("the otput of Num:", Num)
+//let second num is = 1000;
+
+//let 01num = 50; it does not allowed in the beginning
+ //snake_case
+let second_num = 1000; 
+//camelcase
+let secondNum = 1000; 
+
+let decimal_num = 17.6669;
+console.log(decimal_num);
+ // to check data types use 'typeof' operator
+ console.log(typeof decimal_num);
+
+
+ // String:
+ //1. String is a coolection of characters.
+ //2. Srting is a primitive data type.
+
+ let name = "Bishnupriya Nayak";
+ console.log("my name is", name);
+ //``-Backtick
+ console.log(`My name is: ${name}`);
+ console.log(typeof name);
+
+ // Fun fact:
+ let number = "100";
+ console.log(typeof number);//output: string
+
+ let string = 367;
+ console.log(typeof string);//output: number
---
+++ javaScript/class-1/task.js
@@ -0,0 +1,15 @@
+//let num = 30;
+//console.log("The output is:",num);
+
+let num1 = 11;
+let num2 = 12;
+let addition = num1 + num2;
+console.log(addition);
+let substraction = num1 - num2;
+console.log(substraction);
+let multiplication = num1 * num2;
+console.log(multiplication);
+let division = num1 / num2;
+console.log(division);
+let modulus = num1 % num2;
+console.log(modulus);
